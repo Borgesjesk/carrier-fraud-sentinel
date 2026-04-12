a41dd9570eb3076ac14fbd706b34f670ceac9483
BORGES <jessborgesb@gmail.com>
2026-04-12 23:17:19 +0200

    feat: add Transaction domain class with fields and getters

--- src/main/java/com/carrierfraud/domain/Transaction.java
+++ src/main/java/com/carrierfraud/domain/Transaction.java
@@ -1,4 +1,68 @@
 package com.carrierfraud.domain;
 
 public class Transaction {
+
+    private final String carrierName;
+    private final String transportName;
+    private final int failedPayments;
+    private final int succeededPayments;
+    private final double offerPrice;
+    private final int numberOfOffers;
+    private final String incidentExplanation;
+    private final int reviewScore;
+    private final int reportedIncidents;
+    private final boolean missingData;
+
+    public Transaction(String carrierName, String transportName, int failedPayments, int succeededPayments, double offerPrice, int numberOfOffers, String incidentExplanation, int reviewScore, int reportedIncidents, boolean missingData) {
+        this.carrierName = carrierName;
+        this.transportName = transportName;
+        this.failedPayments = failedPayments;
+        this.succeededPayments = succeededPayments;
+        this.offerPrice = offerPrice;
+        this.numberOfOffers = numberOfOffers;
+        this.incidentExplanation = incidentExplanation;
+        this.reviewScore = reviewScore;
+        this.reportedIncidents = reportedIncidents;
+        this.missingData = missingData;
+    }
+
+    public String getCarrierName() {
+        return carrierName;
+    }
+
+    public String getTransportName() {
+        return transportName;
+    }
+
+    public int getFailedPayments() {
+        return failedPayments;
+    }
+
+    public int getSucceededPayments() {
+        return succeededPayments;
+    }
+
+    public double getOfferPrice() {
+        return offerPrice;
+    }
+
+    public int getNumberOfOffers() {
+        return numberOfOffers;
+    }
+
+    public String getIncidentExplanation() {
+        return incidentExplanation;
+    }
+
+    public int getReviewScore() {
+        return reviewScore;
+    }
+
+    public int getReportedIncidents() {
+        return reportedIncidents;
+    }
+
+    public boolean isMissingData() {
+        return missingData;
+    }
 }
